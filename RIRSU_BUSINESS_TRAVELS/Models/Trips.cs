a80acbdafeb74553d6d1a8f759ca6ae4e0fb4400
HarisBeg26 <univerzing.begovic25@gmail.com>
2024-11-23 16:04:41 +0100

    Created a trip model for object mapping

--- RIRSU_BUSINESS_TRAVELS/Models/Trips.cs
+++ RIRSU_BUSINESS_TRAVELS/Models/Trips.cs
@@ -1,6 +1,25 @@
-﻿namespace RIRSU_BUSINESS_TRAVELS.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RIRSU_BUSINESS_TRAVELS.Models
 {
     public class Trips
     {
+        public int Id { get; set; }
+
+
+        [Required]
+        [StringLength(100)]
+        public string Purpose { get; set; }
+
+
+        [Required]
+        [StringLength(100)]
+        public string Destination { get; set; }
+
+        [Required]
+        public DateTime StarDate { get; set; }
+
+        [Required]
+        public DateTime EndDate { get; set; }
     }
 }
